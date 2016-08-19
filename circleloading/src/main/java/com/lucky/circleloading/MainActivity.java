8ab002da4ea0748d5d2cac61b9263ca883d9cc8d
luxiaogang <luxiaogang321@163.com>
2016-08-19 12:11:39 +0800

    circleloadingview

---
+++ circleloading/src/main/java/com/lucky/circleloading/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lucky.circleloading;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CircleLoadingView view= (CircleLoadingView) findViewById(R.id.mCircleLoadingView);
+        view.customAnimation();
+    }
+}
